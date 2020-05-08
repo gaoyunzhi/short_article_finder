8b629a5bafc998648a5331593285616828571613
Yunzhi Gao <gaoyunzhi@gmail.com>
2020-05-08 09:19:20 -0700

    commit

--- short_article_finder.py
+++ short_article_finder.py
@@ -1,34 +1,19 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from telegram_util import log_on_fail
 import time
 import cached_url
 from bs4 import BeautifulSoup
+from preview import getNextPos, getLinks
+from pool import Pool
+from checker import check
 
-with open('channels.txt') as f:
-	pool = f.read().split()
-pool = [x.strip() for x in pool]
-pool = [x for x in pool if x]
+pool = Pool()
 
-def getSoup(name):
-    return BeautifulSoup(cached_url.get('https://telete.in/s/' + name), 'html.parser')
-
-def getMessages():
-    messages = {}
-    for name in pool:
-        soup = getSoup(name)
-        for msg in soup.find_all('div', class_='tgme_widget_message'):
-            msg = Message(msg)
-            if msg.getTitle():
-                messages[msg.getID()] = msg
-    return messages
-
-soup = BeautifulSoup(cached_url.get(LINK_PREFIX + source), 'html.parser')
-	name = soup.find('meta', {'property': 'og:title'})['content']
-	links = {}
-	for item in soup.find_all('a', class_='tgme_widget_message_link_preview'):
-		if 'telegra.ph' not in item['href']:
-			continue
-		title = item.find('div', class_='link_preview_title').text
-		links[item['href']] = title
+while pool.pool:
+	for name, pos in [x, pool.pool[x] for x in pool.pool]:
+		for link in getLinks(name, pos):
+			if check(link):
+				print(link)
+		next_pos = getNextPos(name, pos)
+		pool.update(name, next_pos)
